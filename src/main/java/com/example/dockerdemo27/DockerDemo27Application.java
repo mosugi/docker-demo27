3f45fdfda5a078dabafa53c15b907e2fb2e4e40b
mosugi <mosuging@gmail.com>
2023-05-11 17:55:35 +0900

    Initial commit

---
+++ src/main/java/com/example/dockerdemo27/DockerDemo27Application.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemo27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemo27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemo27Application.class, args);
+    }
+
+}
